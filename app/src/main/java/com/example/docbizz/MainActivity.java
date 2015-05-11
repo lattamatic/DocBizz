16772352cb49a3b2c7d0c6c4aea2ef148f65c0d9
aravind <arvindsuresh2009@gmail.com>
2015-05-12 00:54:10 +0530

    nav drawer is done!!

--- app/src/main/java/com/example/docbizz/MainActivity.java
+++ app/src/main/java/com/example/docbizz/MainActivity.java
@@ -1,39 +1,126 @@
 package com.example.docbizz;
 
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.FrameLayout;
+import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import contacts.ContactItem;
 import contacts.ContactRecyclerViewAdapter;
+import navigationDrawer.NavDrawerItem;
+import navigationDrawer.NavDrawerListAdapter;
 import reports.ReportItem;
 import reports.ReportRecyclerViewAdapter;
 import util.InfiniteRecyclerViewOnScrollListener;
+import util.data;
 
 
 public class MainActivity extends ActionBarActivity {
+
+
+    private DrawerLayout drawerLayout;
+    private ListView drawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+    public static FragmentManager SupportFragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new ReferralsFragment())
-                    .commit();
-        }
+
+        List<NavDrawerItem> list = data.getNavDrawerItems();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.frame_container, new ReferralsFragment());
+        fragmentTransaction.commit();
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerList = (ListView) findViewById(R.id.list_slidermenu);
+
+        drawerList.setAdapter(new NavDrawerListAdapter(getApplicationContext(), list));
+        // Set the list's click listener
+        drawerList.setOnItemClickListener(new DrawerItemClickListener());
+        mDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.app_name, R.string.app_name) {
+
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+        drawerLayout.setDrawerListener(mDrawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_drawer);
+        mDrawerToggle.syncState();
     }
 
+    private void selectItem(int position) {
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        final android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        final Fragment f = fragmentManager.findFragmentById(R.id.frame_container);
+
+            FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(0,0,0);
+            getSupportFragmentManager().findFragmentById(R.id.frame_container)
+                    .getView()
+                    .setLayoutParams(lp);
+
+        switch (position) {
+            case 0 :
+                fragmentTransaction.replace(R.id.frame_container, new SendReferralsFragment());
+                break;
+
+            case 1 :
+                fragmentTransaction.replace(R.id.frame_container, new ReferralsFragment());
+                break;
+
+            case 2 :
+                fragmentTransaction.replace(R.id.frame_container, new ReportFragment());
+                break;
+
+            case 3 :
+                fragmentTransaction.replace(R.id.frame_container, new ContactsFragment());
+                break;
+
+            case 4 :
+                fragmentTransaction.replace(R.id.frame_container, new HelpFragment());
+                break;
+        }
+
+        fragmentTransaction.commit();
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            System.out.println(position);
+            selectItem(position);
+            drawerList.setItemChecked(position, true);
+            drawerLayout.closeDrawer(Gravity.START);
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
