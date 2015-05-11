666c85fa10b645e8235c4fb7d33c04389c9d5885
aravind <arvindsuresh2009@gmail.com>
2015-05-11 15:43:01 +0530

    small change..

--- app/src/main/java/com/example/docbizz/Splashscreen.java
+++ app/src/main/java/com/example/docbizz/Splashscreen.java
@@ -12,6 +12,7 @@ public class Splashscreen extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
     }
 
     @Override
