1142ecc27b19467d171492463724f559585380e5
aravind <arvindsuresh2009@gmail.com>
2015-05-13 13:06:42 +0530

    finished the adapter for the referral recycler view!!

--- app/src/main/java/referrals/ReferralRecyclerViewAdapter.java
+++ app/src/main/java/referrals/ReferralRecyclerViewAdapter.java
@@ -1,6 +1,7 @@
 package referrals;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,7 +10,11 @@ import android.widget.TextView;
 
 import com.example.docbizz.R;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by aravind on 12/5/15.
@@ -26,11 +31,20 @@ public class ReferralRecyclerViewAdapter extends RecyclerView.Adapter<ReferralRe
     }
 
     public final static class ListItemViewHolder extends RecyclerView.ViewHolder {
-        TextView txtDoctorName, txtSentApproved, txtSentDeclined, txtReceivedApproved, txtReceivedDeclined;
+        TextView txtDoctorName, txtReferralTime, txtReferralPatientName, txtReferralStatus;
+        CircleImageView imgDoctorPic;
+
         public ReferralItem item;
 
         public ListItemViewHolder(View itemView) {
             super(itemView);
+
+            imgDoctorPic = (CircleImageView) itemView.findViewById(R.id.circleImagedoctorPic);
+
+            txtDoctorName = (TextView) itemView.findViewById(R.id.txtDoctorName);
+            txtReferralPatientName = (TextView) itemView.findViewById(R.id.txtReferralPatientName);
+            txtReferralStatus = (TextView) itemView.findViewById(R.id.txtReferralStatus);
+            txtReferralTime = (TextView) itemView.findViewById(R.id.txtReferralTime);
         }
     }
 
@@ -48,6 +62,25 @@ public class ReferralRecyclerViewAdapter extends RecyclerView.Adapter<ReferralRe
     public void onBindViewHolder(ListItemViewHolder listItemViewHolder, int position) {
         listItemViewHolder.item = items.get(position);
         ReferralItem item = items.get(position);
+
+        listItemViewHolder.txtDoctorName.setText(item.doctorName);
+        listItemViewHolder.txtReferralTime.setText(item.referralDate);
+        listItemViewHolder.txtReferralPatientName.setText(item.patientName);
+        listItemViewHolder.txtReferralStatus.setText(item.status);
+
+        if(!item.doctorPicURL.equals("")) {
+            //TODO : Set the image of the circle image view to the image at the URL..
+        }
+
+        if(item.status.equals("Declined")) {
+            listItemViewHolder.txtReferralStatus.setTextColor(Color.parseColor("#FF0000"));
+        }
+        else if(item.status.equals("Approved")) {
+            listItemViewHolder.txtReferralStatus.setTextColor(Color.parseColor("#00FF00"));
+        }
+        else if(item.status.equals("Pending")) {
+            listItemViewHolder.txtReferralStatus.setTextColor(Color.parseColor("#000000"));
+        }
     }
 
     public int getItemCount() {
