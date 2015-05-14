cb55f41c240bcc32983949477d11f449b4285929
aravind <arvindsuresh2009@gmail.com>
2015-05-14 20:00:08 +0530

    minor changes..

--- app/src/main/java/com/example/docbizz/Register.java
+++ app/src/main/java/com/example/docbizz/Register.java
@@ -51,7 +51,7 @@ public class Register extends ActionBarActivity {
 
         final Spinner spinnerSpeciality = (Spinner) findViewById(R.id.spinnerSpeciality);
         ArrayAdapter<CharSequence> adapterSpeciality = ArrayAdapter.createFromResource(this,
-                R.array.hospitals_array,android.R.layout.simple_spinner_item);
+                R.array.specialities_array,android.R.layout.simple_spinner_item);
         adapterSpeciality.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerSpeciality.setAdapter(adapterSpeciality);
 
@@ -175,25 +175,4 @@ public class Register extends ActionBarActivity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
